10e96a065027019d0c43b2719a97781ead0b6064
hreypar <hreypar@gmail.com>
2019-07-04 12:57:42 -0500

    add quotes for aspera path

--- 01-prefetch/config.mk
+++ 01-prefetch/config.mk
@@ -1,6 +1,7 @@
 ## Mandatory variables
 MAX_SIZE_FILES="300G"
-ASCP_PATH_AND_KEY="/home/hreyes/.aspera/connect/bin/ascp|/home/hreyes/.aspera/connect/etc/asperaweb_id_dsa.openssh"
+# note the double quotes (required by prefetch)
+ASCP_PATH_AND_KEY='"/home/hreyes/.aspera/connect/bin/ascp|/home/hreyes/.aspera/connect/etc/asperaweb_id_dsa.openssh"'
 
 ## Include if necessary
 #PREFETCH_PATH="/usr/local/share/sratoolkit.2.9.6/bin/prefetch"
