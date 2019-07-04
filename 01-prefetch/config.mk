e83a693582f7f26eb5ca77bd7fcbec62aff1ff6d
hreypar <hreypar@gmail.com>
2019-07-04 12:19:30 -0500

    update config variables

--- 01-prefetch/config.mk
+++ 01-prefetch/config.mk
@@ -1,2 +1,7 @@
-# include if necessary
-#FASTQDUMP_PATH=''
+## Mandatory variables
+MAX_SIZE_FILES="300G"
+ASCP_PATH_AND_KEY="/home/hreyes/.aspera/connect/bin/ascp|/home/hreyes/.aspera/connect/etc/asperaweb_id_dsa.openssh"
+
+## Include if necessary
+#PREFETCH_PATH="/usr/local/share/sratoolkit.2.9.6/bin/prefetch"
+
