d20ce6d5f62abcff06ed316e7358e6b221279da7
hreypar <hreypar@gmail.com>
2019-10-07 14:17:04 -0500

    add asp path

--- 01-prefetch/config.mk
+++ 01-prefetch/config.mk
@@ -1,7 +1,7 @@
 ## Mandatory variables
 MAX_SIZE_FILES="300G"
 # note the double quotes (required by prefetch)
-ASCP_PATH_AND_KEY='"/home/hreyes/.aspera/connect/bin/ascp|/home/hreyes/.aspera/connect/etc/asperaweb_id_dsa.openssh"'
+	ASCP_PATH_AND_KEY='"/home/hreyes/.aspera/connect/bin/ascp|/home/hreyes/.aspera/connect/etc/asperaweb_id_dsa.openssh"'
 
 ## Include if necessary
 #PREFETCH_PATH="/usr/local/share/sratoolkit.2.9.6/bin/prefetch"
